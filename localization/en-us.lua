987569a3ef599340516273350f1e74fcb6e1f7dd
Thermo-Dyn <dillongold05@gmail.com>
2026-06-28 01:44:37 -0700

    (Shadowmantle) Nerf to once per round

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -512,6 +512,7 @@ return {
           "Scored {V:1}#1#{} have a",
           "{C:green}#2# in #3#{} chance to create",
           "a {C:dark_edition}Negative {C:attention}Tag{}",
+	  "Once per round"
         }
       },
       j_paperback_shooting_star = {
