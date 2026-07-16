2b1608ac3b2246837a77ec93b9c113aa8d694648
Thermo-Dyn <dillongold05@gmail.com>
2026-07-15 22:03:03 -0700

    (HATRED) Complete!

--- content/joker/hatred.lua
+++ content/joker/hatred.lua
@@ -35,68 +35,11 @@ SMODS.Joker {
   end,
 
   calculate = function(self, card, context)
-    if context.individual and context.cardarea == G.play then
-      if context.other_card.paperback_hatred_mark then
-        return {
-          xmult = card.ability.extra.xmult
-        }
-      end
-    end
-
-    -- Checks for destroying the marked card
-    if not context.blueprint then
-      local destroyed = false
-      if context.discard and context.other_card.paperback_hatred_mark then
-        card.ability.extra.mark_new = false
-        card.ability.extra.discarded = true
-        return {
-          remove = true,
-          message = localize('paperback_hatred_death_ex'),
-          colour = G.C.MULT
-        }
-      end
-
-      if context.destroy_card and context.cardarea == 'unscored' and context.destroy_card.paperback_hatred_mark then
-        return {
-          remove = true,
-          message = localize('paperback_hatred_death_ex'),
-          colour = G.C.MULT
-        }
-      end
-
-      if context.after then
-        for i, v in ipairs(context.full_hand) do
-          if v.paperback_hatred_mark then
-            card.ability.extra.mark_new = true
-          end
-        end
-        for i, v in ipairs(G.hand.cards) do
-          if v.paperback_hatred_mark then
-            card.ability.extra.mark_new = true
-            SMODS.destroy_cards({ v })
-            destroyed = true
-          end
-        end
-        if card.ability.extra.discarded then
-          card.ability.extra.mark_new = true
-        end
-        for i, v in ipairs(context.scoring_hand) do
-          v.paperback_hatred_mark = nil
-        end
-      end
-      if destroyed then
-        return {
-          message = localize('paperback_hatred_death_ex'),
-          colour = G.C.MULT
-        }
-      end
-    end
-
-    if context.hand_drawn and (context.first_hand_drawn or card.ability.extra.mark_new) then
+    local function mark_card()
       -- Mark a random card that isn't already marked
       local targets = {}
       for _, _card in ipairs(G.hand.cards) do
-        if not card.paperback_hatred_mark then
+        if not card.ability.paperback_hatred_mark then
           targets[#targets + 1] = _card
         end
       end
@@ -105,8 +48,8 @@ SMODS.Joker {
 
       if marked then
         card.ability.extra.mark_new = false
-        marked.paperback_hatred_mark = true
-        juice_card_until(marked, function() return marked.paperback_hatred_mark end, true)
+        marked.ability.paperback_hatred_mark = true
+        juice_card_until(marked, function() return marked.ability.paperback_hatred_mark end, true)
         G.E_MANAGER:add_event(Event {
           trigger = 'immediate',
           delay = 0.5,
@@ -117,11 +60,74 @@ SMODS.Joker {
         })
       end
     end
+
+    if context.individual and context.cardarea == G.play then
+      if context.other_card.ability.paperback_hatred_mark then
+        return {
+          xmult = card.ability.extra.xmult
+        }
+      end
+    end
+
+    -- Checks for destroying the marked card
+    if not context.blueprint then
+      local destroyed = false
+      if context.discard and context.other_card.ability.paperback_hatred_mark then
+        card.ability.extra.mark_new = false
+        card.ability.extra.discarded = true
+        return {
+          remove = true,
+          message = localize('paperback_hatred_death_ex'),
+          colour = G.C.MULT
+        }
+      end
+
+      if context.destroy_card and context.cardarea == 'unscored' and context.destroy_card.ability.paperback_hatred_mark then
+        return {
+          remove = true,
+          message = localize('paperback_hatred_death_ex'),
+          colour = G.C.MULT
+        }
+      end
+
+      if context.after then
+        for i, v in ipairs(context.full_hand) do
+          if v.ability.paperback_hatred_mark then
+            card.ability.extra.mark_new = true
+          end
+        end
+        for i, v in ipairs(G.hand.cards) do
+          if v.ability.paperback_hatred_mark then
+            card.ability.extra.mark_new = true
+            SMODS.destroy_cards({ v })
+            destroyed = true
+          end
+        end
+        if card.ability.extra.discarded then
+          card.ability.extra.mark_new = true
+        end
+        for i, v in ipairs(context.scoring_hand) do
+          v.ability.paperback_hatred_mark = nil
+        end
+        if #G.hand.cards >= G.hand.config.card_limit then
+          mark_card()
+        end
+      end
+      if destroyed then
+        return {
+          message = localize('paperback_hatred_death_ex'),
+          colour = G.C.MULT
+        }
+      end
+      if context.hand_drawn and (context.first_hand_drawn or card.ability.extra.mark_new) then
+        mark_card()
+      end
+    end
   end,
 
   remove_from_deck = function(self, card, from_debuff)
     for i, v in ipairs(G.playing_cards) do
-      v.paperback_hatred_mark = nil
+      v.ability.paperback_hatred_mark = nil
     end
   end
 }
